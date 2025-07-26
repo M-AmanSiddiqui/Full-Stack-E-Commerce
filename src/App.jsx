eb3e85c684dc895ac9ed2feea64fd8f7b65e459a
M-AmanSiddiqui <m.amansidiqui2024@gmail.com>
2025-07-26 05:40:47 -0700

    add top bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-
-
+import React from 'react'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import UserLayout from './components/Layout/UserLayout'
 function App() {
- 
-
   return (
-    <><div>  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-   </div>
-  </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<UserLayout />}> </Route>
+     <Route>{/* Admin Layout */} </Route>
+    
+    </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
---
+++ src/components/Layout/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { IoLogoInstagram } from 'react-icons/io';
+import { RiTwitterXLine } from 'react-icons/ri';
+import { TbBrandMeta } from 'react-icons/tb'
+import { Link } from 'react-router-dom';
+ function Topbar() {
+  return (
+    <div className="bg-[#ea2e0e] text-white">
+      <div className="container mx-auto flex justify-between items-center py-3 px-4">
+      <div className='hidden md:flex items-center space-x-4'>
+        <Link to="#" className="hover:text-gray-300">
+        <TbBrandMeta className="h-5 w-5"/>
+        </Link>
+
+         <Link to="#" className="hover:text-gray-300">
+        <IoLogoInstagram className="h-5 w-5"/>
+        </Link>
+
+         <Link to="#" className="hover:text-gray-300">
+        <RiTwitterXLine className="h-5 w-5"/>
+        </Link>
+     </div>
+     <div className='text-sm text-center flex-grow'>
+        <span>We ship worldwide - Fast and reliable shipping!</span>
+     </div>
+     <div className='text-sm hidden md:block'>
+        <Link to="+923322430504" className='hover:text-gray-300'>+923322430504</Link>
+     </div>
+     </div>
+    </div>
+  )
+}
+export default Topbar
