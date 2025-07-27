3d54e6c2ed7408ef26e8dce517e9aa1f6e884dfd
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-07-27 05:31:41 -0700

    Header Completed

--- src/components/Cart/CartContents.jsx
+++ src/components/Cart/CartContents.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RiDeleteBin3Line } from 'react-icons/ri'
 
  function CartContents() {
 const cartProducts = [
@@ -18,7 +19,7 @@ const cartProducts = [
   size: "L",
   color: "Blue",
   quantity: 24,
-  price: 135,
+  price: 13,
   image: "https://picsum.photos/200?random=2"
 },
 
@@ -27,14 +28,22 @@ const cartProducts = [
   return (
     <div>
       {cartProducts.map((product, index ) => (
-<div key={index} className='fle items-start justify-between py-4 border-b'>
+<div key={index} className='flex items-start justify-between py-4 border-b'>
     <div className='flex items-start'> <img src={product.image} alt={product.name} 
-    className='w-20  h-24 object-cover mr-4 rounded'/>
+    className='w-16  h-20 object-cover mr-4 rounded'/>
 
 <div>
     <h3>{product.name}</h3>
     <p className='text-sm text-gray-500 '>size: {product.size} | color: {product.color}</p>
+    <div className='flex items-center mt-2'>
+        <button className='border rounded px-2 py-1 text-xl font-medium '>-</button>
+        <span className='mx-4'>{product.quantity}</span>
+        <button className='border rounded px-2 py-1 text-xl font-medium '>+</button>
+    </div>
     </div>    
+</div>
+<div><p>$ {product.price.toLocaleString()}</p>
+<button><RiDeleteBin3Line className='h-6 w-6 mt-2 text-red-600'/></button>
 </div>
 </div>
       ))}
